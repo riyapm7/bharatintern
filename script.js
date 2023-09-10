587a45aa36aa239d14438e27d81615ebd7245632
RIYA PRAJAPATI <116753023+riyapm7@users.noreply.github.com>
2023-09-10 17:05:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//script
+
+const celsiusField = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#covert-btn");
+const tempType = document.querySelector("#temp-type");
+
+//windoe leading rest
+
+window.addEventListener("load", () => {
+	degree.value="";
+	celsiusField.innerHTML="";
+
+})
+
+
+
+convertBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	covertToCelsius();
+
+
+	//add loading features
+	convertBtn.innerHTML =
+    "<span><i class='fa fa-spinner fa-spin'></i>Converting....</span>";
+	setTimeout(() =>{
+		convertBtn.innerHTML = "<span>Convert</span>"
+	}, 1000	)
+})
+
+function covertToCelsius(){
+	let inputValue = degree.value;
+
+	setTimeout(() => {
+		if (tempType.value === "fahrenheit") {
+      const fahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+      celsiusField.innerHTML = `${fahrenheitToCelsius.toFixed(3)} &deg;
+		c`;
+    } else if (tempType.value === "kelvin") {
+      const kelvinToCelsius = inputValue - 273.15;
+      celsiusField.innerHTML = `${kelvinToCelsius.toFixed(3)} &deg;
+		c`;
+    }
+	}, 1200)
+}
